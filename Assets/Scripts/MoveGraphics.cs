6bf26adc57b655c517ae5cfb34ffe363e2b9c4d3
Germán Arevalo <germanarevalojerez@gmail.com>
2019-08-25 16:42:20 -0500

    move graphics

---
+++ Assets/Scripts/MoveGraphics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGraphics : MonoBehaviour
+{
+    Vector3 destination = new Vector3(-150, 0, 0); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    public void MoveGraphicPosition()
+    {
+        this.transform.position += destination;
+    }
+}
